044d4ef79672aa930a7a1a8dc9f11023f15608a1
Irfan-vit <shaikmd.irfannawaz2019@vitstudent.ac.in>
2022-01-29 11:56:52 +0530

    added utils
    configured colors, typography, fonts and created a theme with those and set few Global styles

--- src/index.js
+++ src/index.js
@@ -1,16 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { useState } from "react";
-import { defaultTheme, GlobalStyle } from "./utils";
-import { PrimaryButton } from "./components/buttons";
 
 const App = () => {
   return (
     <div>
-      <PrimaryButton modifiers={["warning", "primaryButtonWarning"]}>
-        Hello World
-      </PrimaryButton>
-      <PrimaryButton modifiers={["small"]}>Hello World</PrimaryButton>
       <GlobalStyle />
     </div>
   );
